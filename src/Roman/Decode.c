db2892e7892421bc39c6e8aa2b32afb383adfb98
Raphael KUMAR <etudes.kumar@orange.fr>
2015-03-13 13:23:39 +0100

    Modif initial

--- src/Roman/Decode.c
+++ src/Roman/Decode.c
@@ -1,5 +1,28 @@
 #include "Decode.h"
 
 int roman_decode(char* in) {
-  return 42;
+	int valeur = 0;
+
+	switch ((int)in[1]) {
+        case 73: valeur = 1;
+       	case 86: valeur = 5;
+        case 88: valeur = 10;
+       	case 76: valeur = 50;
+       	case 67: valeur = 100;
+       	case 68: valeur = 500;
+       	case 77: valeur = 1000;
+	}
+
+
+	switch ((int)in[0]) {
+	case 73: return valeur + 1;
+	case 86: return valeur + 5;
+	case 88: return valeur + 10;
+	case 76: return valeur + 50;
+	case 67: return valeur + 100;
+	case 68: return valeur + 500;
+	case 77: return valeur + 1000;
+	default : return -1;
+	}
+
 }
--- tests/Roman/DecodeTest.cpp
+++ tests/Roman/DecodeTest.cpp
@@ -16,8 +16,47 @@ TEST_GROUP(Roman)
   }
 };
 
+//__________Test des Valeur a un seul Caractere__________//
+
 TEST(Roman, Decode_I)
 {
   CHECK_EQUAL(1, roman_decode("I"));
+}
+
+TEST(Roman, Decode_V)
+{
+  CHECK_EQUAL(5, roman_decode("V"));
+}
+
+TEST(Roman, Decode_X)
+{
+  CHECK_EQUAL(10, roman_decode("X"));
+}
+
+TEST(Roman, Decode_L)
+{
+  CHECK_EQUAL(50, roman_decode("L"));
+}
+
+TEST(Roman, Decode_C)
+{
+  CHECK_EQUAL(100, roman_decode("C"));
+}
+
+TEST(Roman, Decode_D)
+{
+  CHECK_EQUAL(500, roman_decode("D"));
+}
+
+TEST(Roman, Decode_M)
+{
+  CHECK_EQUAL(1000, roman_decode("M"));
+}
+
+//__________Test des Valeur a 2 caractère en somme__________//
+
+TEST(Roman, Decode_II)
+{
+  CHECK_EQUAL(2, roman_decode("II"));
 }
 
